1de1b8608b5409255007c4b147c2c42a489e7421
zhe chen <wztxy89@163.com>
2024-05-31 01:43:33 +0800

    Update interleave pretrain

--- internvl_chat_dev/shell/interleaved_pretrain/internvl_chat_v1_5_internlm2_7b_448_res_pretrain_interleaved_stage1.sh
+++ internvl_chat_dev/shell/interleaved_pretrain/internvl_chat_v1_5_internlm2_7b_448_res_pretrain_interleaved_stage1.sh
@@ -1,17 +1,16 @@
 set -x
 
 PARTITION=${PARTITION:-"INTERN2"}
-GPUS=${GPUS:-512}
+GPUS=${GPUS:-504}
 GPUS_PER_NODE=${GPUS_PER_NODE:-8}
 QUOTA_TYPE=${QUOTA_TYPE:-"reserved"}
 NODES=$((GPUS / GPUS_PER_NODE))
 CPUS_PER_TASK=${CPUS_PER_TASK:-10}
 SRUN_ARGS=${SRUN_ARGS:-""}
-BATCH_SIZE=${BATCH_SIZE:-8192}
+BATCH_SIZE=${BATCH_SIZE:-8064}
 PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
-export PYTHONPATH="/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/petrel-oss-python-sdk"
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
@@ -22,10 +21,10 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
-# number of gpus: 512
+# number of gpus: 504
 # batch size per gpu: 4
 # gradient accumulation steps: 4
-# total batch size: 8192
+# total batch size: 8064
 # epoch: 1
 srun -p ${PARTITION} \
   --gres=gpu:${GPUS_PER_NODE} \
@@ -60,8 +59,8 @@ srun -p ${PARTITION} \
   --gradient_accumulation_steps ${GRADIENT_ACC} \
   --evaluation_strategy "no" \
   --save_strategy "steps" \
-  --save_steps 100 \
-  --save_total_limit 20 \
+  --save_steps 200 \
+  --save_total_limit 100 \
   --learning_rate 2e-5 \
   --weight_decay 0.05 \
   --warmup_steps 300 \
