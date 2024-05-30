275e8a3c2b62fced3ebdbb134f78c8f9f8cb41b8
zhe chen <wztxy89@163.com>
2024-05-30 12:32:26 +0800

    Update shell

--- internvl_chat_dev/tools/unittest_chat_data_v3.py
+++ internvl_chat_dev/tools/unittest_chat_data_v3.py
@@ -335,7 +335,7 @@ def test_dataset(dataset):
 
 if __name__ == '__main__':
     # llm_path = './pretrained/Meta-Llama-3-8B-Add-Token'
-    llm_path = './pretrained/Phi-3-mini-128k-instruct'
+    llm_path = './pretrained/Qwen1.5-72B-Chat'
     llm_tokenizer = AutoTokenizer.from_pretrained(
         llm_path, add_eos_token=False, trust_remote_code=True, use_fast=False)
     token_list = [IMG_START_TOKEN, IMG_END_TOKEN, IMG_CONTEXT_TOKEN,
@@ -350,7 +350,7 @@ if __name__ == '__main__':
         print(f'Testing {ds_name}...')
         dataset = LazySupervisedDataset(
             # 'llama3-chat',
-            'phi3-chat',
+            'Hermes-2',
             ds_collections[ds_name],
             llm_tokenizer,
             tcs_loader,
