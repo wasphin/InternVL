f1aeec215f42426d5222a32d2f0e3c340d524e86
zhe chen <wztxy89@163.com>
2024-05-31 23:46:26 +0800

    Update shell

--- internvl_chat_dev/shell/internlm2_100b_dynamic/internvl_chat_v1_5_internlm2_100b_dynamic_res_pretrain.sh
+++ internvl_chat_dev/shell/internlm2_100b_dynamic/internvl_chat_v1_5_internlm2_100b_dynamic_res_pretrain.sh
@@ -7,24 +7,25 @@ QUOTA_TYPE=${QUOTA_TYPE:-"reserved"}
 NODES=$((GPUS / GPUS_PER_NODE))
 CPUS_PER_TASK=${CPUS_PER_TASK:-10}
 SRUN_ARGS=${SRUN_ARGS:-""}
-BATCH_SIZE=${BATCH_SIZE:-8192}
-PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
+BATCH_SIZE=${BATCH_SIZE:-2048}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-2}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
+export PYTHONPATH="/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/petrel-oss-python-sdk"
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
 
-OUTPUT_DIR='work_dirs/interleaved/internvl_chat_v1_5_internlm2_7b_448_res_pretrain_interleaved_stage1'
+OUTPUT_DIR='work_dirs/internvl_chat_v1_5/internvl_chat_v1_5_internlm2_100b_dynamic_res_pretrain'
 
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
 # number of gpus: 512
-# batch size per gpu: 4
-# gradient accumulation steps: 4
-# total batch size: 8192
+# batch size per gpu: 2
+# gradient accumulation steps: 2
+# total batch size: 2048
 # epoch: 1
 srun -p ${PARTITION} \
   --gres=gpu:${GPUS_PER_NODE} \
@@ -35,44 +36,44 @@ srun -p ${PARTITION} \
   --kill-on-bad-exit=1 \
   --quotatype=${QUOTA_TYPE} \
   ${SRUN_ARGS} \
-  python -u internvl/train/internvl_chat_pretrain_interleaved.py \
-  --vision_path "./pretrained/InternViT-300M-448px" \
-  --llm_path "./pretrained/internlm2-chat-7b" \
+  python -u internvl/train/internvl_chat_pretrain.py \
+  --vision_path "./pretrained/InternViT-6B-448px-V1-5" \
+  --mlp_path "./pretrained/InternViT-6B-448px-V1-5/mlp_projector/internlm2_chat_100b.pth" \
+  --llm_path "./pretrained/b330k-e29k-sft-l1-0413" \
   --conv_style "internlm2-chat" \
   --output_dir ${OUTPUT_DIR} \
-  --meta_path "./shell/data/cc_interleave_pretrain_stage1.json" \
+  --meta_path "./shell/data/data_0404_zh_pretrain_tcluster.json" \
   --overwrite_output_dir True \
   --force_image_size 448 \
-  --max_dynamic_patch 1 \
   --down_sample_ratio 0.5 \
   --drop_path_rate 0.0 \
   --pad2square False \
-  --freeze_llm False \
+  --freeze_llm True \
   --freeze_mlp False \
   --freeze_backbone True \
   --vision_select_layer -1 \
   --use_data_resampling False \
-  --dataloader_num_workers 12 \
+  --dataloader_num_workers 8 \
   --bf16 True \
   --num_train_epochs 1 \
   --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
   --gradient_accumulation_steps ${GRADIENT_ACC} \
   --evaluation_strategy "no" \
   --save_strategy "steps" \
-  --save_steps 200 \
-  --save_total_limit 100 \
-  --learning_rate 2e-5 \
+  --save_steps 100 \
+  --save_total_limit 3 \
+  --learning_rate 1e-5 \
   --weight_decay 0.05 \
-  --warmup_steps 300 \
+  --warmup_steps 100 \
   --lr_scheduler_type "cosine" \
   --logging_steps 1 \
-  --max_seq_length 2048 \
+  --max_seq_length 4096 \
   --do_train True \
   --grad_checkpoint True \
   --group_by_length False \
-  --dynamic_image_size False \
-  --use_thumbnail False \
+  --dynamic_image_size True \
+  --use_thumbnail True \
   --ps_version 'v2' \
-  --deepspeed "zero_stage1_config.json" \
+  --deepspeed "zero_stage3_config_72b.json" \
   --report_to "tensorboard" \
   2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
