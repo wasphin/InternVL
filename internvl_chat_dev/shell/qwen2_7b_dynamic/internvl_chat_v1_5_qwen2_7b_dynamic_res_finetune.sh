0004688edc4e0a82e6704d828911d790ba4176d4
zhe chen <wztxy89@163.com>
2024-06-10 16:14:08 +0800

    Update shell

--- internvl_chat_dev/shell/qwen2_7b_dynamic/internvl_chat_v1_5_qwen2_7b_dynamic_res_finetune.sh
+++ internvl_chat_dev/shell/qwen2_7b_dynamic/internvl_chat_v1_5_qwen2_7b_dynamic_res_finetune.sh
@@ -1,31 +1,31 @@
 set -x
 
 PARTITION=${PARTITION:-"INTERN2"}
-GPUS=${GPUS:-640}
+GPUS=${GPUS:-256}
 GPUS_PER_NODE=${GPUS_PER_NODE:-8}
 QUOTA_TYPE=${QUOTA_TYPE:-"reserved"}
 NODES=$((GPUS / GPUS_PER_NODE))
 CPUS_PER_TASK=${CPUS_PER_TASK:-10}
 SRUN_ARGS=${SRUN_ARGS:-""}
-BATCH_SIZE=${BATCH_SIZE:-1280}
-PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-2}
+BATCH_SIZE=${BATCH_SIZE:-1024}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
-export PYTHONPATH="/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/petrel-oss-python-sdk"
+
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
 
-OUTPUT_DIR='work_dirs/internvl_chat_v1_5/internvl_chat_v1_5_qwen2_72b_dynamic_res_finetune'
+OUTPUT_DIR='work_dirs/internvl_chat_lite/internvl_chat_v1_5_qwen2_7b_dynamic_res_finetune'
 
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
-# number of gpus: 640
-# batch size per gpu: 2
+# number of gpus: 256
+# batch size per gpu: 4
 # gradient accumulation steps: 1
-# total batch size: 1280
+# total batch size: 1024
 # epoch: 1
 srun -p ${PARTITION} \
   --gres=gpu:${GPUS_PER_NODE} \
@@ -37,21 +37,21 @@ srun -p ${PARTITION} \
   --quotatype=${QUOTA_TYPE} \
   ${SRUN_ARGS} \
   python -u internvl/train/internvl_chat_finetune.py \
-  --model_name_or_path "./work_dirs/internvl_chat_v1_5/internvl_chat_v1_5_qwen2_72b_dynamic_res_pretrain/checkpoint-3400" \
+  --model_name_or_path "./work_dirs/internvl_chat_lite/internvl_chat_v1_5_qwen2_7b_dynamic_res_pretrain/checkpoint-9800" \
   --conv_style "Hermes-2" \
   --output_dir ${OUTPUT_DIR} \
   --meta_path "./shell/data/data_yi34b_finetune_v5_33.json" \
   --overwrite_output_dir True \
   --force_image_size 448 \
   --down_sample_ratio 0.5 \
-  --drop_path_rate 0.4 \
+  --drop_path_rate 0.1 \
   --pad2square False \
   --freeze_llm False \
   --freeze_mlp False \
   --freeze_backbone False \
   --vision_select_layer -1 \
   --use_data_resampling False \
-  --dataloader_num_workers 6 \
+  --dataloader_num_workers 3 \
   --bf16 True \
   --num_train_epochs 1 \
   --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
@@ -72,6 +72,6 @@ srun -p ${PARTITION} \
   --dynamic_image_size True \
   --use_thumbnail True \
   --ps_version 'v2' \
-  --deepspeed "zero_stage3_config_72b.json" \
+  --deepspeed "zero_stage1_config.json" \
   --report_to "tensorboard" \
   2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
