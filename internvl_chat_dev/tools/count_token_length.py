2bb4b4f3bb1dc06cbb0b98ed18a09397dd6ce80b
zhe chen <wztxy89@163.com>
2024-06-15 18:14:46 +0800

    Clean code

--- internvl_chat_dev/tools/count_token_length.py
+++ internvl_chat_dev/tools/count_token_length.py
@@ -1,4 +1,4 @@
-import json as json
+import json
 import os
 from concurrent.futures import ProcessPoolExecutor
 
@@ -7,40 +7,32 @@ from internvl.train.dataset import TCSLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
-
-def is_chinese(text):
-    return any('\u4e00' <= char <= '\u9fff' for char in text)
-
-
+# Initialize the dataset loader
 tcs_loader = TCSLoader('~/petreloss.conf')
-try_to_load_image = True
 
-# set your path and output path here
-# this code will find all `.jsonl` files and count the token length
-path = '/mnt/hwfile/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/metas/medical_data/VQA/'
-output = '/mnt/hwfile/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/metas/stage3_v5_20240611_std/medical_vqa/'
+# Set your path and output path here
+# This code will find all `.jsonl` files and count the token length
+path = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/metas/stage3_v5_20240611_std/mmmu_test'
+output = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/metas/stage3_v5_20240611_std/mmmu_test2'
 
+# Model path for the tokenizer
 model_path = '/mnt/hwfile/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/work_dirs/internvl_chat_v1_5/' \
              'internvl_chat_v1_5_internlm2_20b_dynamic_res_finetune_exp7_26'
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, use_fast=False)
 
-meta_path = './shell/data/data_yi34b_finetune_v5_35.json'
+# Load metadata
+meta_path = './shell/data/data_yi34b_finetune_v5_42.json'
 meta = json.load(open(meta_path, 'r'))
-basename2meta = {}
-for k, v in meta.items():
-    annotation = v['annotation']
-    basename = os.path.basename(annotation)
-    basename2meta[basename] = v
+basename2meta = {os.path.basename(v['annotation']): v for k, v in meta.items()}
 
+# Collect all file paths
 file_paths = []
-# list all files in the directory and its subdirectories
 for root, dirs, files in os.walk(path):
     for file in files:
         file_path = os.path.join(root, file)
-        # ignore temp directories and files
         if '_temp' not in file_path:
             file_paths.append(file_path)
-file_paths = [f for f in file_paths if f.endswith('internvl.jsonl')]
+file_paths = [f for f in file_paths if f.endswith('.jsonl')]
 
 
 def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
@@ -59,26 +51,95 @@ def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_
     return best_ratio
 
 
+def process_image(line, root):
+    if 'image' in line:
+        if isinstance(line['image'], list):
+            if len(line['image']) == 1:
+                line['image'] = line['image'][0]
+                return process_single_image(line, root)
+            else:
+                return process_image_list(line, root)
+        else:
+            return process_single_image(line, root)
+    return line
+
+
+def process_image_list(line, root):
+    if 'width_list' not in line or 'height_list' not in line:
+        width_list, height_list, image_list = [], [], []
+        for url in line['image']:
+            image_path = os.path.join(root, url)
+            try:
+                image = tcs_loader(image_path)
+                width, height = image.size
+                width_list.append(width)
+                height_list.append(height)
+                image_list.append(url)
+            except:
+                continue
+        if not image_list:
+            return None
+        line['image'] = image_list
+        line['width_list'] = width_list
+        line['height_list'] = height_list
+        line['width'] = width_list[0]
+        line['height'] = height_list[0]
+    return line
+
+
+def process_single_image(line, root):
+    image_path = os.path.join(root, line['image'])
+    if 'width' not in line or 'height' not in line:
+        try:
+            image = tcs_loader(image_path)
+            width, height = image.size
+            line['width'] = width
+            line['height'] = height
+        except:
+            pass
+    return line
+
+
+def calculate_image_tokens(line, image_size=448):
+    width_list = line.get('width_list', [line['width']])
+    height_list = line.get('height_list', [line['height']])
+    num_patch_total = 0
+    num_image = len(width_list)
+    for width, height in zip(width_list, height_list):
+        aspect_ratio = width / height
+        min_num, max_num = 1, 12 // num_image
+        target_ratios = {(i, j) for n in range(min_num, max_num + 1) for i in range(1, n + 1) for j in range(1, n + 1) if i * j <= max_num and i * j >= min_num}
+        target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+        best_ratio = find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size=image_size)
+        num_patch = best_ratio[0] * best_ratio[1]
+        if num_patch != 1:
+            num_patch += 1  # Add a thumbnail
+        num_patch_total += num_patch
+    image_token = num_patch_total * 256
+    image_count = num_patch_total
+    return image_token, image_count
+
+
+def tokenize_conversations(line):
+    try:
+        conversations = line['conversations']
+    except KeyError:
+        raise ValueError('Conversations key missing in the line')
+    conversations = '\n'.join([item['value'] for item in conversations])
+    tokenized = tokenizer(conversations, return_tensors='pt', padding=False, max_length=99999, truncation=True).input_ids
+    text_token = tokenized.shape[1]
+    return text_token
+
+
 def process_file(file_path):
     basename = os.path.basename(file_path)
-    if basename in basename2meta:
-        meta = basename2meta[basename]
-        root = meta['root']
-    else:
-        root = ''
+    root = basename2meta.get(basename, {}).get('root', '')
 
     output_path = file_path.replace(path, output)
-    # mkdir dir for output_path
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     writer = open(output_path, 'w')
 
     diff_length = []
-    min_num, max_num = 1, 12
-    target_ratios = set(
-        (i, j) for n in range(min_num, max_num + 1) for i in range(1, n + 1) for j in range(1, n + 1) if
-        i * j <= max_num and i * j >= min_num)
-    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
-
     try:
         with open(file_path, 'r') as f:
             data = f.readlines()
@@ -86,93 +147,31 @@ def process_file(file_path):
                 try:
                     line = json.loads(line)
                 except:
-                    # 解析失败直接跳过
                     continue
-                # 删掉多余的image_root
-                if 'image_root' in line:
-                    del line['image_root']
-                # image只有多余1条时才能用list
-                if 'image' in line and type(line['image']) == list and len(line['image']) == 1:
-                    line['image'] = line['image'][0]
-                if 'image' in line and type(line['image']) == str:
-                    line['image'] = line['image'].replace('s3://medical_preprocessed/', '')
+
+                line = process_image(line, root)
+                if line is None:
+                    continue
 
                 if 'image' in line:
-                    if type(line['image']) == list:
-                        pass
-                    else:
-                        # 如果是单图的数据，并且长宽未知时，去读取数据获取长宽
-                        image_path = os.path.join(root, line['image'])
-                        if 'width' not in line or 'height' not in line:
-                            try:
-                                image = tcs_loader(image_path)
-                                width, height = image.size
-                                line['width'] = width
-                                line['height'] = height
-                            except:
-                                # 读取失败时跳过
-                                pass
-
-                    # 多图先直接按12算
-                    width = line['width'] if 'width' in line else 448 * 3
-                    height = line['height'] if 'height' in line else 448 * 4
-                    aspect_ratio = width / height
-                    best_ratio = find_closest_aspect_ratio(
-                        aspect_ratio, target_ratios, width, height, image_size=448)
-                    num_patch = best_ratio[0] * best_ratio[1]
-
-                    if num_patch == 1:
-                        image_token = 256  # 只有1个块时，没有缩略图
-                        image_count = 1
-                    else:
-                        if type(line['image']) == list:
-                            image_count = len(line['image'])
-                            image_token = image_count * 256
-                            # 如果有多图，每个都是448x448
-                        else:
-                            image_token = (num_patch + 1) * 256  # add a thumbnail
-                            image_count = num_patch + 1
-                            # 否则按动态分辨率统计
+                    image_token, image_count = calculate_image_tokens(line)
                 else:
-                    image_token = 0
-                    image_count = 0
-                try:
-                    conversations = line['conversations']
-                except:
-                    print(file_path)
-                    exit()
-                # 医疗影像VQA预处理
-                for conv in conversations:
-                    if conv['from'] == 'human':
-                        if is_chinese(conv['value']):
-                            conv['value'] = conv['value'] + '\n请简要回答，不要出现多余的内容。'
-                        else:
-                            conv['value'] = conv[
-                                                'value'] + '\nPlease answer briefly, without including extraneous information.'
-                    if 'Please generate a caption for this medical image' in conv['value']:
-                        conv['value'] = conv['value'].replace(
-                            '\nPlease answer briefly, without including extraneous information.', '')
-                        conv['value'] = conv['value'].replace('Please generate a caption for this medical image.',
-                                                              'Please generate a short caption for this medical image.')
-                conversations = '\n'.join([item['value'] for item in conversations])
-                tokenized = tokenizer(
-                    conversations, return_tensors='pt', padding=False, max_length=99999, truncation=True).input_ids
-                text_token = tokenized.shape[1]
-                if 'length' in line:
-                    diff_length.append(image_token + text_token - line['length'])
-                else:
-                    diff_length.append(0)
+                    image_token, image_count = 0, 0
+
+                text_token = tokenize_conversations(line)
+
+                diff_length.append(image_token + text_token - line.get('length', 0))
                 line['length'] = image_token + text_token
                 line['image_count'] = image_count
                 writer.write(json.dumps(line, ensure_ascii=False) + '\n')
         writer.close()
         diff_length = np.array(diff_length).mean()
         print(f'{basename} finished! diff: {diff_length}')
-    except:
+    except Exception as e:
         import traceback
         traceback.print_exc()
 
 
-# 使用ProcessPoolExecutor并行处理文件
+# Use ProcessPoolExecutor to process files in parallel
 with ProcessPoolExecutor() as executor:
     list(tqdm(executor.map(process_file, file_paths), total=len(file_paths)))
