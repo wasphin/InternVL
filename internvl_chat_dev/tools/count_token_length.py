c1b71794a0e6747fb3249187bc3381e12705462e
zhe chen <wztxy89@163.com>
2024-06-18 14:40:10 +0800

    Update data

--- internvl_chat_dev/tools/count_token_length.py
+++ internvl_chat_dev/tools/count_token_length.py
@@ -12,8 +12,8 @@ tcs_loader = TCSLoader('~/petreloss.conf')
 
 # Set your path and output path here
 # This code will find all `.jsonl` files and count the token length
-path = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/data/infographics_v4/annotations'
-output = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/data/infographics_v4/annotations2'
+path = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/data/chemistry_data/annotations'
+output = '/mnt/petrelfs/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/metas/stage3_v5_20240611_std/chemistry'
 
 # Model path for the tokenizer
 model_path = '/mnt/hwfile/wangweiyun/workspace_cz/InternVL/internvl_chat_dev/work_dirs/internvl_chat_v1_5/' \
